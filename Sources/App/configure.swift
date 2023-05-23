c9c73f6c196b4e740663d36b2f318e1e01bb33f0
VerticalHeretic <ls.warss98@gmail.com>
2023-05-23 15:03:57 +0200

    Implemented first models and endpoints, without controller yet

--- Sources/App/configure.swift
+++ Sources/App/configure.swift
@@ -17,8 +17,11 @@ public func configure(_ app: Application) async throws {
         tls: .prefer(try .init(configuration: .clientDefault)))
     ), as: .psql)
 
-    app.migrations.add(CreateTodo())
+    app.migrations.add(CreateAcronym())
+    
+    app.logger.logLevel = .debug
 
-    // register routes
+    try await app.autoMigrate()
+
     try routes(app)
 }
